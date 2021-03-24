21c83e58df90c8b61d6827291704c141abd98754
Pepe Pótamo <jesus.alvite.naval@gmail.com>
2021-03-24 08:23:59 +0100

    Para los bateras de piso mejor la electronica

---
+++ Piso.py
@@ -0,0 +1,7 @@
+Vamos a realizar una serie de libros que nos ayuden a aprender a tocar la bateria acustica
+Las baquetas se han de agarrar haciendo pinza con el pulgar e indice y acompañando con el resto de los dedos
+La posicion de baerista será con la espalda derecha, hombros relajados y la caja a la altura de las piernas.
+Primeros golpes. Aprenderemos a tocar piano, medio y forte
+El rebote de la baqueta que vuelve siempre al mismo lugar.
+
+si tocas en un apartamento te recomiendo la bateria electronica

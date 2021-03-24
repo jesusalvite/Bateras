2882e9d7bd4f2b048d87672638320386853de1a3
Pepe Pótamo <jesus.alvite.naval@gmail.com>
2021-03-24 08:15:24 +0100

    La posicion del baterista

--- PrimeraClase.py
+++ PrimeraClase.py
@@ -1,1 +1,3 @@
 Vamos a realizar una serie de libros que nos ayuden a aprender a tocar la bateria acustica
+Las baquetas se han de agarrar haciendo pinza con el pulgar e indice y acompañando con el resto de los dedos
+La posicion de baerista será con la espalda derecha, hombros relajados y la caja a la altura de las piernas.
